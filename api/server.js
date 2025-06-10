49d7663f5bcb44b20195922ee65152d2fbba6508
Erik Brandon <erikbrandon1@gmail.com>
2025-06-10 18:34:54 -0400

    Separated package.json, .env & Dockerized App - In `api/.env.example`, created a .env file containing all the   environment variables needed for the backend API. - In `api/Dockerfile`, created a 2-stage Dockerfile that builds the   React frontend and copies it into the filesystem of the API container   so the API can serve the React bundle. - In `api/middlewares/index.js`, updated the Express static call to   serve the React app from a `public` folder. - In `api/package.json` & `api/package-lock.json`, created a separate   package.json file containing scripts and dependencies specific to the   backend API. - In `api/server.js`, updated the Express sendFile call to serve the   index.html file from the public folder containing the React app   bundle. Also updated the app.all catch-all to include a required splat   parameter used in the latest version of Express. - In `docker-compose.yaml`, created a root level docker-compose   containing the application and a MongoDB container to simplify the   local development and deployment process. - In `package.json` & `package-lock.json`, deleted the root-level   package.json files because they are no longer needed in the monorepo   structure. - In `reactjs/.env.example`, created a .env file containing all the   environment variables needed for the frontend React application. - In `reactjs/package.json` & `reactjs/package-lock.json`, created a   separate package.json file containing scripts and dependencies   specific to the frontend React application. - In `reactjs/vite.config.js`, removed the envDir value from the Vite   config because the .env file is no longer located within the parent   directory.
    Signed-off-by: Erik Brandon <erikbrandon1@gmail.com>

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const { middlewares } = require('./middlewares/index');
-const { validateEnv } = require('./utils/validateEnv');
-const { connectDB } = require('./db/config');
+const { middlewares } = require("./middlewares/index");
+const { validateEnv } = require("./utils/validateEnv");
+const { connectDB } = require("./db/config");
 
-const spotifyRouter = require('./routes/spotifyRouter');
+const spotifyRouter = require("./routes/spotifyRouter");
 
 const { PORT } = validateEnv();
 
@@ -14,16 +14,16 @@ const app = express();
 middlewares(app);
 connectDB();
 
-app.use('/spotify/v1', spotifyRouter);
+app.use("/spotify/v1", spotifyRouter);
 
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../reactjs/dist', 'index.html'));
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
 });
 
-app.all('/*', (req, res) => {
-    res.redirect('/');
+app.all("/{*splat}", (req, res) => {
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
-    console.log(`Backend Server Running > PORT = ${PORT}`);
+  console.log(`Backend Server Running > PORT = ${PORT}`);
 });
